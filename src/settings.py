b313cf66f258b710f728545566aafda7fe316ba6
WalPur <mishapython@yandex.ru>
2024-06-04 15:26:08 +0900

    отключил пагинацию

--- src/settings.py
+++ src/settings.py
@@ -147,8 +147,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 SUMMERNOTE_CONFIG = {"attachment_absolute_uri": True}
-
-REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
-}
